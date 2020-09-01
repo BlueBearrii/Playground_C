3374138cd583e9972457009f5724ef0efb5e28ed
bluebearrii <thananan.math.acs@gmail.com>
2020-09-01 23:52:41 +0700

    :seedling: Variables

---
+++ Variables.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ void main() {
+     int setInt;
+     float setFloat;
+     char setChar;
+     char setCharString[10]; // 🔥 Set string variable and set size 10 character
+     char *setCharStringPointer; // 🔥 set string variable with pointer => did not declear char size []
+     double setDouble;
+
+     printf("Test print value : something"); // Test print value 
+ }
